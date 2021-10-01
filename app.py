2a8ceb39b72d55a7a2f802f0bf9c8e2c7e42421a
Harsh Devmurari <harshdevmurari007@gmail.com>
2021-10-01 12:08:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,173 @@
+import os
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from datetime import datetime
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd, password_check
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+app.jinja_env.filters["usd"] = usd
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+conn = sqlite3.connect("database.db")
+db = conn.cursor()
+
+try:
+    sqliteConnection = sqlite3.connect('database.db', check_same_thread=False)
+    db = sqliteConnection.cursor()
+    print("Database created and Successfully Connected to SQLite")
+
+    sqlite_select_Query = "select sqlite_version();"
+    db.execute(sqlite_select_Query)
+    record = db.fetchall()
+    print("SQLite Database Version is: ", record)
+    
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+
+
+#db.execute("""CREATE TABLE users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(id))""")
+#db.execute("""CREATE TABLE users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(id))""")
+print("AAAAAAAAAAAAAAAAAAAAAAAa")
+db.execute("SELECT * FROM users")
+print(db.fetchall())
+print("table succesfull")
+
+"""
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
+"""
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Log user in
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        db.execute("SELECT * FROM users WHERE username = ?", [request.form.get("username")])
+        rows = db.fetchall()
+        print(rows)
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # Log user out
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # Register user
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Creat variables
+        username = request.form.get("username")
+        type(username)
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure user has written username
+        if not username:
+            return apology("Must provide Username", 400)
+
+        # Ensure no other user is in database with same username
+        db.execute("SELECT * FROM users WHERE username = ?", (username,))
+        rows = db.fetchall()
+        print(f"TYPE : ...............{type(rows)}")
+        if len(rows) != 0:
+            return apology("Username is already taken")
+
+        # Ensure user has written password
+        if not password:
+            return apology("Must provide password", 400)
+
+        # Ensure user has written confirmation of password
+        if not confirmation:
+            return apology("Must provide confirmation", 400)
+
+        # Ensure password in both fields(password and confirmation) is same
+        if password != confirmation:
+            return apology("Password and Confirmation donot match")
+
+        # Ensure password satisfies the requirements of one uppercase, one lowercase, one numeric and one symbol
+        ps = list(password.strip(" "))
+        requirement = password_check(ps)
+        if requirement == False:
+            return apology("Password Requirement")
+
+        # Register user by storing it in database. Here, javascript can be added.
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", [username, generate_password_hash(password)])
+        print("BBBBBBBBB")
+        print(db.fetchall())
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link)
+    else:
+        return render_template("register.html")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,89 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+# Apology renders memetemplate as an userside error
+def apology(message, code=400):
+    # Render message as an apology to user.
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+# Function used above every other function which ensures user is logged in
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# Function inserts symbol to url which inturn returns all info about that stock
+def lookup(symbol):
+    # Look up quote for symbol.
+
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
+
+# Functions which converts float to usd
+def usd(value):
+    # Format value as USD.
+    return f"${value:,.2f}"
+
+
+# Checks password requirements
+def password_check(ps):
+    # Ensures password contains atleast one uppercase, one lowercase, one numeric.
+    uc = False
+    lc = False
+    nm = False
+    print(type(uc))
+    for char in ps:
+        if char.isupper():
+            uc = True
+        if char.islower():
+            lc = True
+        if char.isdigit():
+            nm = True
+
+    if uc and lc and nm:
+        return True
+    else:
+        return False
